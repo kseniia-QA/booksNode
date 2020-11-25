a0961dc6bb4b1521159bc2d2dfb7d2e4b280c3ab
Alex Kulagin <al.qlagin@gmail.com>
2020-11-25 18:29:33 +0500

    extract add book form to component

--- src/components/BooksDeck.jsx
+++ src/components/BooksDeck.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import { Container, Form, Button, CardDeck, Modal } from 'react-bootstrap';
+import { CardDeck, Container } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import Book from './Book';
 
 export default function BooksDeck({
-  show,
-  openForm,
-  closeForm,
-  AddBookCard,
-  bookCover,
-  changeForm,
-  fail,
-  sendForm,
-  cards,
-}) {
+                                    openForm,
+                                    AddBookCard,
+                                    cards,
+                                  }) {
   return (
     <>
       <Container>
@@ -25,84 +19,13 @@ export default function BooksDeck({
           <AddBookCard openForm={openForm} />
         </CardDeck>
       </Container>
-      <Modal show={show} onHide={closeForm}>
-        <Modal.Header closeButton>
-          <Modal.Title>Book description</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form
-            onChange={(event) => changeForm(event)}
-            onSubmit={(event) => sendForm(event)}
-          >
-            <Form.Group controlId="title">
-              <Form.Label>Title</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter book title"
-                required
-              />
-            </Form.Group>
-            <Form.Group controlId="description">
-              <Form.Label>Description</Form.Label>
-              <Form.Control type="text" placeholder="Enter book description" />
-            </Form.Group>
-            <Form.Group controlId="fileCover">
-              <Form.Label>Cover</Form.Label>
-              <div>
-                <img
-                  src={bookCover}
-                  alt="book cover"
-                  style={{ maxWidth: '100%' }}
-                />
-                <Form.File />
-              </div>
-              <div
-                className={`mb-3 pl-4 text-danger font-weight-bold collapse ${fail.class}`}
-              >
-                {fail.message}
-              </div>
-            </Form.Group>
-            <Form.Group controlId="fileBook">
-              <Form.Label>Book file</Form.Label>
-              <Form.File />
-            </Form.Group>
-            <Form.Group controlId="authors">
-              <Form.Label>Authors</Form.Label>
-              <Form.Control type="text" placeholder="Enter book authors" />
-            </Form.Group>
-            <Form.Group>
-              <Form.Check
-                label="add to favorite"
-                id="favorite"
-                feedbackTooltip
-              />
-            </Form.Group>
-            <Button variant="dark" onClick={closeForm}>
-              Cansel
-            </Button>
-            <Button className="ml-2" variant="success" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Modal.Body>
-      </Modal>
     </>
   );
 }
 
 BooksDeck.propTypes = {
   openForm: PropTypes.func.isRequired,
-  closeForm: PropTypes.func.isRequired,
-  show: PropTypes.bool.isRequired,
   AddBookCard: PropTypes.func,
-  bookCover: PropTypes.string.isRequired,
-  // changeBookCover: PropTypes.func.isRequired,
-  changeForm: PropTypes.func.isRequired,
-  fail: PropTypes.shape({
-    class: PropTypes.string,
-    message: PropTypes.string,
-  }).isRequired,
-  sendForm: PropTypes.func.isRequired,
   // eslint-disable-next-line react/forbid-prop-types
   cards: PropTypes.any.isRequired,
 };
--- src/components/BooksHandler.jsx
+++ src/components/BooksHandler.jsx
@@ -1,67 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import defaultBookCover from '../img/book';
+import { Modal } from 'react-bootstrap';
 import BooksDeck from './BooksDeck';
 import AddBookCard from './AddBookCard';
+import AddBookForm from './AddBookForm';
 
 export default function BooksHandler() {
-  const [show, formShow] = useState(false); // показ добавления книги
-  const openForm = () => formShow(true);
-  const closeForm = () => formShow(false);
+  const [books, setBooks] = useState([]);
+  const [isFormShown, showForm] = useState(false); // показ добавления книги
+  const openForm = () => showForm(true);
+  const closeForm = () => showForm(false);
 
-  const [fail, setFail] = useState({ class: '', message: '' }); // показ ошибки при неправильной обложке
-  const invalidImage = () => {
-    setFail({ class: 'show', message: 'Неправильный тип файла' });
-    setTimeout(() => setFail({ class: '', message: null }), 5000);
-  };
-
-  const [bookCover, setBookCover] = useState(defaultBookCover);
-
-  const formData = new FormData();
-  formData.append('title', '');
-  formData.append('description', '');
-  formData.append('authors', '');
-  formData.append('favorite', '');
-  formData.append('fileCover', bookCover);
-
-  const [form] = useState(formData);
-  const changeForm = async (event) => {
-    const { id } = event.target;
-    if (event.target.files) {
-      const files = event.target.files[0];
-      if (!files) return;
-
-      const filesType = files.type.split('/')[0];
-
-      if (filesType !== 'image' && id === 'fileCover') {
-        invalidImage();
-        return;
-      }
-
-      const reader = new FileReader();
-      reader.readAsDataURL(files);
-      reader.onload = () => {
-        if (id === 'fileCover') {
-          form.set('fileCover', reader.result);
-          setBookCover(reader.result);
-        } else {
-          form.set('fileBook', reader.result);
-          form.set('fileName', files.name);
-        }
-      };
-    } else if (id === 'favorite') {
-      if (form.get('favorite')) {
-        form.set('favorite', '');
-      } else {
-        form.set('favorite', true);
-      }
-    } else {
-      form.set(`${id}`, `${event.target.value}`);
-    }
-  };
-
-  const sendForm = async (event) => {
-    event.preventDefault();
+  const fetchBooks = () => {
+    const url = `${process.env.REACT_APP_URL}/api/books`;
+    fetch(url)
+      .then((response) => response.json())
+      .then(setBooks)
+  }
+  const sendForm = async (form) => {
     const url = `${process.env.REACT_APP_URL}/api/books`;
     form.set('key', uuidv4());
 
@@ -71,32 +27,34 @@ export default function BooksHandler() {
     });
 
     if (response.ok) {
-      // window.location.reload();
+      closeForm();
+      await fetchBooks();
     } else {
       throw Error(response.statusText);
     }
   };
-  const [cards, setCards] = useState([]);
-  useEffect(() => {
-    const url = `${process.env.REACT_APP_URL}/api/books`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setCards(data);
-      });
-  }, []);
+  useEffect(fetchBooks, []);
 
   return (
-    <BooksDeck
-      show={show}
-      openForm={openForm}
-      closeForm={closeForm}
-      AddBookCard={AddBookCard}
-      bookCover={bookCover}
-      changeForm={changeForm}
-      fail={fail}
-      sendForm={sendForm}
-      cards={cards}
-    />
+    <>
+      <BooksDeck
+        openForm={openForm}
+        AddBookCard={AddBookCard}
+        cards={books}
+      />
+      {isFormShown && (
+        <Modal show={isFormShown} onHide={closeForm}>
+          <Modal.Header closeButton>
+            <Modal.Title>Book description</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <AddBookForm
+              closeForm={closeForm}
+              sendForm={sendForm}
+            />
+          </Modal.Body>
+        </Modal>
+      )}
+    </>
   );
 }
